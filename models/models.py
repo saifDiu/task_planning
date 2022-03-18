2a3f2cf83b7c4121b2893e297bfc86eb1d20846c
saifDiu <alsaif839@gmail.com>
2022-03-18 11:07:58 +0600

    Update

--- models/models.py
+++ models/models.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
-
-
-class task_planning(models.Model):
-    _inherit = 'res.partner'
-
-    type_of_product = fields.Char(string="Type of Product")
-    terms = fields.Char(string="Terms")
+#from odoo import models, fields, api
+#
+#
+#class task_planning(models.Model):
+#    _inherit = 'res.partner'
+#
+#    type_of_product = fields.Char(string="Type of Product")
+#    terms = fields.Char(string="Terms")
 
